acf50097ac7e8829329fac10c6c9399ba1d6ca0d
Rober <rober@mail.js-tech.tw>
2020-04-25 19:03:53 +0800

    [His]雜湊碼移除

--- historic.php
+++ historic.php
@@ -18,20 +18,21 @@
         <META http-equiv=Content-Type content="text/html; charset=utf-8">
     </HEAD>
 <body>
-<br>f86644f test
-<br>02df904 [網路設定]test
-<br>5314068 後續使用
-<br>3b23b4e 加入測試SQL
-<br>61688f4 [修正資料庫]
-<br>ea67898 DEMO
-<br>838e4f7 [移除]刪掉不要的套件
-<br>370d070 [調整]初版完成
-<br>a93c3ee [排程][頁面]選取頁面新增
-<br>ea932f8 [排程]寫入資料成功
-<br>319b837 [Model]加入資料庫連線
-<br>cef2028 期數改成統一方式
-<br>ac50c2b [爬蟲]加入新增資料庫語法
-<br>42d9488 [爬蟲]撈資料test版0420
-<br>73ae636 [套件]相關事項(如沒用到後續考慮移除)
+<br>[新增]跟球走功能、button css樣式新增、日期判斷修正
+<br>test
+<br>[網路設定]test
+<br>後續使用
+<br>加入測試SQL
+<br>[修正資料庫]
+<br>DEMO
+<br>[移除]刪掉不要的套件
+<br>[調整]初版完成
+<br>[排程][頁面]選取頁面新增
+<br>[排程]寫入資料成功
+<br>[Model]加入資料庫連線
+<br>期數改成統一方式
+<br>[爬蟲]加入新增資料庫語法
+<br>[爬蟲]撈資料test版0420
+<br>[套件]相關事項(如沒用到後續考慮移除)
 </body>
 </HTML>
